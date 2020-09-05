b2c915103ef771b5bfb7b0612651905a4d56e477
Neena K <neenanarayanan97@gmail.com>
2020-09-05 17:29:24 +0530

    New content added

--- swaping.py
+++ swaping.py
@@ -3,5 +3,7 @@ b = input('Enter value of y:')
 temp = a
 a = b
 b = temp
-print('The value of a after swapping:{}.format(x)')
-print('The value of b after swapping:{}.format(y)')
+ghghjj
+jjhkjkj
+kklkl
+nkjk
