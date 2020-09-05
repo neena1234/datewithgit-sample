bd35ab151719f1862c172f2bf8a9ba8dbb22e4e3
Neena K <neenanarayanan97@gmail.com>
2020-09-05 16:43:38 +0530

    Second commit

---
+++ swaping.py
@@ -0,0 +1,7 @@
+a = input('Enter value of x:')
+b = input('Enter value of y:')
+temp = a
+a = b
+b = temp
+print('The value of a after swapping:{}.format(x)')
+print('The value of b after swapping:{}.format(y)')
